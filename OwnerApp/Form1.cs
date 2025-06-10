de318a522700d497a9eda70b7dea7cfdd5ffeabc
Tan <aliyaliyaa00@gmail.com>
2025-06-11 04:40:08 +0700

    add OwnerApp: initial stucture

---
+++ OwnerApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace OwnerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- OwnerApp/obj/Debug/net8.0-windows/OwnerApp.AssemblyInfo.cs
+++ OwnerApp/obj/Debug/net8.0-windows/OwnerApp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("OrderTrack")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("OwnerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c723a673867fab54b8196f26b54503e6899823e7")]
-[assembly: System.Reflection.AssemblyProductAttribute("OrderTrack")]
-[assembly: System.Reflection.AssemblyTitleAttribute("OrderTrack")]
+[assembly: System.Reflection.AssemblyProductAttribute("OwnerApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("OwnerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
