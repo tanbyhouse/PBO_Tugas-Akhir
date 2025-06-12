fbb1c53ddc687c0ee5aeb6b951e08844bf280aae
Tan <aliyaliyaa00@gmail.com>
2025-06-12 21:11:06 +0700

    update UI card produk, add quantity control

---
+++ OrderTrackCustomer/OrderTrack/view/UserControls/QuantityBox.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace OrderTrack.view.UserControls
+{
+    partial class QuantityBox
+    {
+        private Button btnMinus;
+        private Button btnPlus;
+        private Label lblValue;
+
+        private void InitializeComponent()
+        {
+            btnMinus = new Button();
+            btnPlus = new Button();
+            lblValue = new Label();
+            SuspendLayout();
+
+            // btnMinus
+            btnMinus.Dock = DockStyle.Left;
+            btnMinus.FlatAppearance.BorderColor = Color.Tan;
+            btnMinus.Font = new Font("Gilroy-Medium", 10F);
+            btnMinus.ForeColor = Color.FromArgb(77, 59, 47);
+            btnMinus.Location = new Point(0, 0);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(30, 30);
+            btnMinus.TabIndex = 2;
+            btnMinus.Text = "-";
+            btnMinus.Click += btnMinus_Click;
+
+            // btnPlus
+            btnPlus.Dock = DockStyle.Right;
+            btnPlus.FlatAppearance.BorderColor = Color.Tan;
+            btnPlus.Font = new Font("Gilroy-Medium", 10F);
+            btnPlus.ForeColor = Color.FromArgb(77, 59, 47);
+            btnPlus.Location = new Point(125, 0);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(30, 30);
+            btnPlus.TabIndex = 1;
+            btnPlus.Text = "+";
+            btnPlus.Click += btnPlus_Click;
+
+            // label Value
+            lblValue.Dock = DockStyle.Fill;
+            lblValue.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblValue.Location = new Point(30, 0);
+            lblValue.Name = "lblValue";
+            lblValue.Size = new Size(95, 30);
+            lblValue.TabIndex = 0;
+            //lblValue.Text = "0";
+            lblValue.TextAlign = ContentAlignment.MiddleCenter;
+
+            // QuantityBox
+            Controls.Add(lblValue);
+            Controls.Add(btnPlus);
+            Controls.Add(btnMinus);
+            Name = "QuantityBox";
+            Size = new Size(155, 30);
+            ResumeLayout(false);
+        }
+    }
+}
---
+++ OrderTrackCustomer/OrderTrack/view/UserControls/QuantityBox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderTrack.view.UserControls
+{
+    public partial class QuantityBox : UserControl
+    {
+        public event EventHandler ValueChanged;
+        public int Minimum { get; set; } = 0;
+        public int Maximum { get; set; } = 200;
+
+        public int Value
+        {
+            get => int.Parse(lblValue.Text);
+            set
+            {
+                int val = Math.Max(Minimum, Math.Min(Maximum, value));
+                lblValue.Text = val.ToString();
+                btnMinus.Enabled = val > Minimum;
+                btnPlus.Enabled = val < Maximum;
+            }
+        }
+        public QuantityBox()
+        {
+            InitializeComponent();
+            Value = 0;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (Value > Minimum)
+            {
+                Value--;
+                ValueChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (Value < Maximum)
+            {
+                Value++;
+                ValueChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
