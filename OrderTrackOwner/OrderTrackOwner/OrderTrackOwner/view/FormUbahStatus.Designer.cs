74cb38e6428338163a663c64613d9074bacd28f1
Tan <aliyaliyaa00@gmail.com>
2025-06-15 22:33:09 +0700

    add new form: Ubah Status

---
+++ OrderTrackOwner/OrderTrackOwner/OrderTrackOwner/view/FormUbahStatus.Designer.cs
@@ -0,0 +1,70 @@
+﻿using OrderTrackOwner.Data.Context_s;
+
+namespace OrderTrackOwner.view
+{
+    partial class FormPilihStatus
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.comboBoxStatus = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxStatus
+            // 
+            this.comboBoxStatus.FormattingEnabled = true;
+            this.comboBoxStatus.Location = new System.Drawing.Point(30, 30);
+            this.comboBoxStatus.Name = "comboBoxStatus";
+            this.comboBoxStatus.Size = new System.Drawing.Size(200, 28);
+            this.comboBoxStatus.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(240, 30);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(94, 29);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // FormPilihStatus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 100);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.comboBoxStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPilihStatus";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Pilih Status Pesanan";
+            this.Load += new System.EventHandler(this.FormPilihStatus_Load);
+            this.ResumeLayout(false);
+        }
+
+        private void FormPilihStatus_Load(object sender, EventArgs e)
+        {
+            comboBoxStatus.DataSource = Enum.GetValues(typeof(StatusPesanan));
+        }
+        #endregion
+
+        private ComboBox comboBoxStatus;
+        private Button btnOK;
+    }
+}
---
+++ OrderTrackOwner/OrderTrackOwner/OrderTrackOwner/view/FormUbahStatus.cs
@@ -0,0 +1,46 @@
+﻿using OrderTrackOwner.Data.Context_s;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace OrderTrackOwner.view
+{
+    public partial class FormPilihStatus : Form
+    {
+        public StatusPesanan StatusTerpilih { get; private set; }
+        public int Id_Pesanan { get; set; }
+
+        public FormPilihStatus()
+        {
+            InitializeComponent();
+            comboBoxStatus.DataSource = Enum.GetValues(typeof(StatusPesanan));
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            StatusTerpilih = (StatusPesanan)comboBoxStatus.SelectedItem;
+
+            using (var db = new AppDbContext_s())
+            {
+                var pesananGrid = db.Pesanan.Find(this.Id_Pesanan);
+                if (pesananGrid != null)
+                {
+                    pesananGrid.StatusPesanan = StatusTerpilih;
+                    db.SaveChanges(); // Penting: simpan perubahan ke database
+                }
+            }
+
+            // Tambahkan event agar UC_DaftarPesanan bisa refresh data
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+    }
+}
